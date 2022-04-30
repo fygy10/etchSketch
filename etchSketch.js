e97bdffcdc2778fe06010b8e14accdb5fac3743e
fygy10 <niko.timothyl@gmail.com>
2022-04-30 11:25:41 -0400

    Etch with mouse cursor with a set time to disappear

--- etchSketch.js
+++ etchSketch.js
@@ -1,24 +1,19 @@
 const container = document.getElementById('grid')
-
 const mouseMovement = document.createElement('div')
-mouseMovement.setAttribute('id', mouseMovement);
 mouseMovement.classList.add('mouseMovement');
-
-mouseMovement.insertBefore(mouseMovement, body.firstChild);
+container.insertBefore(mouseMovement, container.firstChild);
 
 createGrid ();
 
 function createGrid(){
-    for(i = 0; i < 16; i++){
+    for(i = 0; i <= 1600; i++){
         const square = createSquare ();
         mouseMovement.appendChild(square);
     }
-
 }
 
 function createSquare (){
-    const div = document.createElement('div');
-    div.classList.add('square');
-    return div;
-
+    const content = document.createElement('div');
+    content.classList.add('square');
+    return content;    
 }
